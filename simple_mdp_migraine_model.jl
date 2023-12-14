8cfdb29d90539435f4061fd5d3b115bc4b4c1265
castrong <strongcreativesystems@gmail.com>
2023-12-14 14:28:28 -0800

    temp notebook

---
+++ simple_mdp_migraine_model.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.17.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 3af902c0-a29d-44aa-83bb-92842adabdd9
+i'm new here!
+
+# ╔═╡ 1d65e1f3-d458-4175-82eb-7a47b6e45bc4
+
+
+# ╔═╡ Cell order:
+# ╠═3af902c0-a29d-44aa-83bb-92842adabdd9
+# ╠═1d65e1f3-d458-4175-82eb-7a47b6e45bc4
